4ea80b6e0336c06cbf4d8aa19ca8b3c849f17886
王辉 <wangh_0929@163.com>
2020-07-08 15:05:14 +0800

    优化

--- src/test/java/com/example/StringByteArrayExamples.java
+++ src/test/java/com/example/StringByteArrayExamples.java
@@ -14,26 +14,27 @@ class StringByteArrayExamples {
         //Original String
         String string = "hello world";
 
-        //Convert to byte[]
         byte[] bytes = string.getBytes();
-
-        //Convert back to String
         String s = new String(bytes);
-
-        //Check converted string against original String
         System.out.println("Decoded String : " + s);
 
         byte[] stringBytes = string.getBytes(StandardCharsets.UTF_8);
         String s2 = new String(stringBytes,StandardCharsets.UTF_8);
         System.out.println("Decoded String(utf-8) :" + s2);
 
+        if (bytes != null && bytes.length > 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : bytes) {
+                // 以十六进制输出
+                stringBuilder.append(String.format("%X", b));
+            }
+            System.out.println("hello hex：" + stringBuilder);
+        }
 
         //Base64 Encoded
         String encoded = Base64.getEncoder().encodeToString(bytes);
-
         //Base64 Decoded
         byte[] decoded = Base64.getDecoder().decode(encoded);
-
         //Verify original content
         System.out.println("Decoded String(Base64) : " +new String(decoded));
 
